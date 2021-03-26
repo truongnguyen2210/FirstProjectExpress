6210be699a17e7610db34f253d857476310a959a
truongnguyen <truongthanhnguyen2210@gmail.com>
2021-03-26 15:56:36 +0700

    #13 middleware

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -20,20 +20,6 @@ module.exports.create = function(req, res) {
 };
 module.exports.doCreate = function(req, res) {
     req.body.id = shortid.generate();
-    var errors = [];
-    if (!req.body.name) {
-        errors.push("Name is requied.");
-    }
-    if (!req.body.phone) {
-        errors.push("Phone is requied.");
-    }
-    if (errors.length) {
-        res.render("users/create", {
-            errors: errors,
-            values: req.body
-        });
-        return;
-    }
     db.get('users').push(req.body).write();
     res.redirect('/users');
 };
--- routers/user.route.js
+++ routers/user.route.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controller/user.controller');
+var validate = require('../validate/user.validate');
+
 
 router.get("/", controller.index);
 router.get("/search", controller.search);
 router.get("/create", controller.create);
 
 router.get("/:id", controller.viewUser);
-router.post("/create", controller.doCreate);
+router.post("/create", validate.postCreate, controller.doCreate);
 
 module.exports = router;
